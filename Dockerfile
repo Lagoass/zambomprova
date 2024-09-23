3776e83e8404e07f62954cb78667868c5b8e9c2f
Lagoass <gustavog6@al.insper.edu.br>
2024-09-23 09:57:49 -0300

    reseet

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM amazoncorretto:21
-COPY target/prova-0.0.1-SNAPSHOT.jar /usr/app/
-ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/br/insper/prova/Jogador/JogadorRepository.java
+++
@@ -1,6 +0,0 @@
-package br.insper.prova.Jogador;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JogadorRepository extends MongoRepository<Jogador, String> {
-}
