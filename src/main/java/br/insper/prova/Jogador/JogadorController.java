e5d27a9e1121ce0a25216f7b0ca422caf98a4f4b
Lagoass <gustavog6@al.insper.edu.br>
2024-09-23 09:55:31 -0300

    a

--- src/main/java/br/insper/prova/Jogador/JogadorController.java
+++ src/main/java/br/insper/prova/Jogador/JogadorController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Jogador")
+@RequestMapping("/jogador")
 public class JogadorController {
     @Autowired
     private JogadorService jogadorService;
